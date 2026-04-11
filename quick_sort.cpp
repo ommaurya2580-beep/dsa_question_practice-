04c9bff04622b2ff5515bdaa8eab8e97c71b69e6
Om Maurya <ommaurya2580@gmail.com>
2026-04-11 21:26:28 +0530

    update

---
+++ quick_sort.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[low];
+    int start = low + 1;
+    int end = high;
+
+    while (start <= end) {
+
+        while (start <= high && arr[start] < pivot) {
+            start++;
+        }
+
+        while (arr[end] > pivot) {
+            end--;
+        }
+
+        if (start < end) {
+            swap(arr[start], arr[end]);
+            start++;
+            end--;
+        }
+    }
+
+    swap(arr[low], arr[end]);
+    return end;  
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low >= high) return;
+
+    int p = partition(arr, low, high);
+
+    quickSort(arr, low, p - 1);
+    quickSort(arr, p + 1, high);
+}
+
+int main() {
+    vector<int> arr = {71,62,13,42,59,68,72,84,21};
+
+    quickSort(arr, 0, arr.size() - 1);
+
+    for (int x : arr) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
